fa7e16a7e06c3ea0cce50324bb656b6ff28297a3
Antonio Sebastiano <sebastianoa@hotmail.com>
2021-09-26 22:49:54 +1000

    first card layout

--- src/components/CardDisplayFront.js
+++ src/components/CardDisplayFront.js
@@ -1,33 +1,34 @@
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+
 import chip from "../images/chip.png";
 
 function CardDisplayFront({ cardName, cardNumber, expMonth, expYear, cvv }) {
   return (
     <div className="card-front p-3 mt-5">
-        <Row className="justify-content-between mt-1">
-            <div className="cc-imgs ms-2">
-                <img src={chip} alt="creditcard chip" className="chip m-0"></img>
-            </div>
+        <Row className="justify-content-end mt-1">
             <div className="cc-imgs">
             LOGO
             </div>
         </Row>
-      <Row className="justify-content-center mt-4 mb-3">
-        {!cardNumber ? "0000 0000 0000 0000" : (
-        <p className="text-center">{cardNumber}</p>
-        )}
+        <Row>
+        <div className="cc-imgs ms-2 ">
+                <img src={chip} alt="creditcard chip" className="chip mt-1"></img>
+            </div>
+        </Row>
+      <Row className="justify-content-center mt-1 mb-1 card-number">
+        <p className="text-center">{!cardNumber ? "0000 0000 0000 0000" : cardNumber}</p>
       </Row>
       <Row className="mb-0">
-        <Col className="mb-0">
-          <div>
-            <p className="mb-0">Card Holder</p>
-            <p>{cardName}</p>
-          </div>
+        <Col xs={9} className="">
+          <Row className="card-holder">
+            <p className="mb-1">Card Holder</p>
+            <p className="mb-1">{!cardName ? "XXXXX XXXXXXXX" : cardName.toUpperCase()}</p>
+          </Row>
         </Col>
-        <Col className="mb-0">
+        <Col xs={2} className="mb-0">
           <div>
-            <p className="mb-0">Expires</p>
+            <p className="mb-1">Expires</p>
             <p>
               <span>{!expMonth ? "MM" : expMonth}</span>/<span>{!expYear ? "YY" : (expYear).toString().slice(-2)}</span>
             </p>
