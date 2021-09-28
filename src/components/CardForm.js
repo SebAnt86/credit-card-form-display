d794a0b583560a94f66de0a5cebe4c07404ce238
Antonio Sebastiano <sebastianoa@hotmail.com>
2021-09-28 16:05:01 +1000

    add card-number validation with patterns

--- src/components/CardForm.js
+++ src/components/CardForm.js
@@ -63,6 +63,39 @@ function CardForm({
       setCardNumValid(false);
     }
     //console.log(cardType);
+
+    const patternMatch = (cardNumber) => {
+      const regexPattern = {
+        MASTERCARD: /^5[1-5][0-9]{1,}|^2[2-7][0-9]{1,}$/,
+        VISA: /^4[0-9]{2,}$/,
+        AMERICAN_EXPRESS: /^3[47][0-9]{5,}$/,
+        DISCOVER: /^6(?:011|5[0-9]{2})[0-9]{3,}$/,
+        DINERS_CLUB: /^3(?:0[0-5]|[68][0-9])[0-9]{4,}$/,
+        JCB: /^(?:2131|1800|35[0-9]{3})[0-9]{3,}$/,
+      };
+      for (const card in regexPattern) {
+        if (cardNumber.replace(/[^\d]/g, "").match(regexPattern[card])) {
+          if (cardNumber) {
+            return cardNumber &&
+              /^[1-6]{1}[0-9]{14,15}$/i.test(
+                cardNumber.replace(/[^\d]/g, "").trim()
+              )
+              ? ""
+              : "Enter a valid Card";
+          }
+        }
+      }
+      return "Enter a valid Card";
+    };
+
+    //alert(patternMatch(cardNumber));
+    if(patternMatch(cardNumber)){
+      setCardNumValid(true);
+      setCardNumErr("Please enter a valid card number.");
+    } else {
+      setCardNumValid(false);
+    }
+
   };
 
   const cardNameValidation = () => {
